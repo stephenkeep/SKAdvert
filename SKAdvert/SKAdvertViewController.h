1bcafceba193daa7ad92315ab5c184576787df92
Stephen Keep <stephen@keep-creative.com>
2012-03-30 13:27:04 +0100

    first commit

---
+++ SKAdvert/SKAdvertViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKAdvertViewController.h
+//  SKAdvertisement
+//
+//  Created by Stephen Keep on 30/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKAdvertViewController : UIViewController
+
+@end
