b8019396fd29d91a9d171f64d19ea86f6d5f1277
Stephen Keep <stephen@keep-creative.com>
2012-03-30 12:09:47 +0100

    Initial Commit

---
+++ SKAdvert/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SKAdvert
+//
+//  Created by Stephen Keep on 30/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
